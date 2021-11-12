5ddeea8cff763fe6039da06b29c991ecfb488c39
amit9793 <as9455@srmist.edu.in>
2021-11-12 20:14:50 +0530

    submitibg crickbuzz clone website

---
+++ search.js
@@ -0,0 +1,33 @@
+async function search(){
+    var a1 = JSON.parse(localStorage.getItem("find"));
+    
+    let response = await fetch(
+        `https://newsapi.org/v2/everything?q=${a1}&apiKey=e0f50a912c704931a073a03da9d3fe0e`
+    );
+    let data = await response.json();
+     console.log(data)
+
+    for (let title of data.articles) {
+        let x1 = title.title;
+        
+        let y1 = title.urlToImage;
+        
+        var pic = document.createElement("img");
+        pic.style="width:400px;height:200px; border: black;"
+        pic.src=y1;
+       
+        var name = document.createElement("h2");
+        name.style = "height:50px;text-align:center;margin-top:90px;margin-left:50px"
+        name.innerHTML = x1;
+
+        var combo = document.createElement("div");
+        combo.style="display:flex;margin-top:15px"
+        combo.append(pic,name);
+
+        var poster = document.getElementById("newspage");
+        poster.append(combo);
+        
+    }
+
+}
+search();
